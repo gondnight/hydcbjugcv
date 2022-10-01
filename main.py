7bf9ac995b580006aad0a7b7febab109aed87c57
gondnight <114292657+gondnight@users.noreply.github.com>
2022-10-01 07:18:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
+bot_token=os.getenv('TOKEN')
 bot=discord.Client(command_prefix="g")
 
 @bot.command()
 async def wew(ctx):
     await ctx.send(".")
-bot.run(TOKEN)
+bot.run(bot_token)
